90ff0f34878f2bcbea96bc1883b28720c7bae1da
Mateus Gomesgit <mateus.liderprojetos@gmail.com>
2020-04-23 14:19:43 -0300

    change solved status

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -44,7 +44,7 @@ class Index extends Component {
 
       return { page, posts, pages };
     } catch (err) {
-      if (err.data.status === 403) {
+      if (err.data && err.data.status === 403) {
         tokenExpired();
       }
     }
